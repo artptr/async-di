6830243ed31b93ed302bf67f38e9143c5ef52c84
Artem Petrenkov <static.lab@gmail.com>
2023-08-01 09:44:19 +0300

    move DI implementation to 'lib'

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import { Container } from './Container.js';
+import { Container } from './lib/Container.js';
 
-const loader = serviceName => {
-    return import(`./${serviceName.replaceAll('.', '/')}.js`).then(module => module.default);
-};
+const container = new Container();
 
-const container = new Container(loader);
 const { 'app.Blog': blog } = container.injector();
+
 container.ready().then(() => {
     blog.post('Hello World');
 });
